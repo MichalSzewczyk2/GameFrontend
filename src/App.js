a210c9a027c912bc7618daa1f9cb9297fbce72ce
Michał <michal.szewczyk.200@gmail.com>
2023-11-22 21:15:09 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Register from "./pages/register/registerForm"
+import Game from "./pages/game/game";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Register/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/game" element={<Game/>}/>
         </Routes>
     );
 }
--- src/pages/register/registerForm.js
+++ src/pages/register/registerForm.js
@@ -1,16 +1,12 @@
 import React, {useState} from "react";
 
 function Register() {
-    // Używam stanu, aby przechowywać dane wprowadzone przez użytkownika
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
 
-    // Funkcja, która obsługuje wysłanie formularza
     async function handleSubmit(e) {
-        // Zapobiegam domyślnej akcji przeglądarki
         e.preventDefault();
-        // Wyświetlam dane użytkownika w konsoli
 
         const permission = 1;
 
@@ -34,26 +30,23 @@ function Register() {
             },
             body: JSON.stringify(user)
         }).then((response) => {
-            // Check if the response is ok
             if (response.status.valueOf() === 201) {
-                // Return the response as JSON
+
                 return response.json();
             } else {
-                // Throw an error
+
                 throw new Error("Something went wrong");
             }
         })
             .then((user) => {
-                // Handle the data
                 console.log(user);
             })
             .catch((error) => {
-                // Handle the error
                 console.error(error);
             });
 
 
-        // Czyszczę pola formularza
+
         setEmail("");
         setPassword("");
         setUsername("");
