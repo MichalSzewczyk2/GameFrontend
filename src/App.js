bc4d70889fae58f6b37762a8448005d85b3ce0cb
Michał <michal.szewczyk.200@gmail.com>
2023-11-22 23:28:34 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import {Route, Routes} from "react-router-dom";
 import Register from "./pages/register/registerForm"
 import Game from "./pages/game/game";
+import Menu from "./components/elements/menu"
+import Login from "./pages/login/login";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        console.log('Getting info about the user')
+        fetch('/user/logged', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Got user info', data)
+                setUser(data)
+            })
+            .catch(error => {
+                console.log('Error getting user info', error)
+            })
+    }, [])
     return (
-        <Routes>
-            <Route path="/" element={<Register/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/game" element={<Game/>}/>
-        </Routes>
+        <div>
+            <Menu user={user} setUser={setUser}/>
+            <Routes>
+                <Route path="/" element={<Register/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/game" element={<Game/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </div>
     );
 }
 
--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,8 +1,38 @@
 import React, {useState, useEffect} from "react";
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import MenuItem from '@mui/material/MenuItem'
+import Checkbox from "@mui/material/Checkbox";
+import Chip from '@mui/material/Chip';
+import ListItemText from '@mui/material/ListItemText'
+
 
 const App = () => {
 
     const [data, setData] = useState([]);
+    const [sortType, setSortType] = useState(null)
+    const [selectedAge, setSelectedAge] = useState([])
+
+    const ages = [3, 7, 12, 16, 18];
+
+    const sortOptions = [
+        {value: 'alpha_asc', label: 'a->z'},
+        {value: 'alpha_desc', label: 'z->a'},
+        {value: 'year_asc', label: 'Oldest'},
+        {value: 'year_desc', label: 'Newest'}
+    ]
+
+    const handleAgeChange = event => {
+        const {
+            target: {value},
+        } = event
+        setSelectedAge(
+            typeof value === 'string' ? value.split(',') : value,
+        )
+    }
 
     const fetchData = async () => {
         const response = await fetch("http://127.0.0.1:8080/game/");
@@ -11,16 +41,84 @@ const App = () => {
     };
 
     useEffect(() => {
+        const sortGames = (a, b) => {
+            if (sortType === 'alpha_asc') return a.title.toUpperCase().localeCompare(b.title.toUpperCase())
+            if (sortType === 'alpha_desc') return b.title.toUpperCase().localeCompare(a.title.toUpperCase())
+            if (sortType === 'year_asc') return a.release_date - b.release_date
+            if (sortType === 'year_desc') return b.release_date - a.release_date
+            return 0
+        }
+        setData(datas => [...datas].sort(sortGames))
+    }, [sortType]);
+
+    useEffect(() => {
         fetchData();
     }, []);
 
+    const filteredData = data.filter(
+        data =>
+            (selectedAge.length === 0 || selectedAge.includes(data.age_rating))
+    )
+
     return (
         <div>
-            {data.map((item) => (
-                <li key={item.id}>
-                    <h2>{item.title}</h2>
-                </li>
-            ))}
+            <div className='game-filter-sort'>
+                <FormControl className='form-control'>
+                    <InputLabel className='input-label'> Age </InputLabel>
+                    <Select
+                        className='select'
+                        labelId='filter-by-ageRating'
+                        id='filter-by-ageRating'
+                        multiple
+                        value={selectedAge}
+                        onChange={handleAgeChange}
+                        input={<OutlinedInput id='select-multiple-chip' label='Brand'/>}
+                        renderValue={selected =>
+                            <Box>
+                                {selected.map(value =>
+                                    <Chip key={value} label={value}/>,
+                                )}
+                            </Box>
+                        }
+                    >
+                        {ages.map(age =>
+                            <MenuItem
+                                className='menu-item'
+                                key={age}
+                                value={age}
+                            >
+                                <Checkbox checked={selectedAge.indexOf(age) > -1}/>
+                                <ListItemText primary={age}/>
+                            </MenuItem>,
+                        )}
+                    </Select>
+                </FormControl>
+                <FormControl className='game-sort form-control'>
+                    <InputLabel className='input-label' id='sort-by-type'>Sort by</InputLabel>
+                    <Select
+                        labelId='sort-by-type'
+                        id='sort-by-type'
+                        value={sortType}
+                        onChange={e => setSortType(e.target.value)}
+                    >
+                        {sortOptions.map(option =>
+                            <MenuItem
+                                key={option.value}
+                                value={option.value}
+                            >
+                                {option.label}
+                            </MenuItem>,
+                        )}
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                {filteredData.map((item) => (
+                    <ul key={item.id}>
+                        <div>{item.title}, {item.platform}, {item.description}, {item.system_requirements}, {item.genre}, {item.release_date}, {item.developer}, {item.age_rating}, {item.publisher}, {item.addition_date}</div>
+                    </ul>
+                ))}
+            </div>
         </div>
     );
 }
